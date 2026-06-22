225bdb7d7422e17d826e960dca524c0b038a8a2d
RohmanXmonitoring <adityrama022012@gmail.com>
2026-06-22 20:06:54 +0700

    Update serverActions configuration in next.config.js
    Changed serverActions from a boolean to an object with allowedOrigins.

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ['firebasestorage.googleapis.com'],
   },
   experimental: {
-    serverActions: true,
+    serverActions: {
+      // Ubah dari boolean ke object
+      allowedOrigins: ['localhost:3000', 'top-saldo.vercel.app'],
+    },
   },
   env: {
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
