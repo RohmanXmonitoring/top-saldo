ef09b8fe82e15b259cd06cf6c286ac33be0937a4
RohmanXmonitoring <adityrama022012@gmail.com>
2026-06-22 20:17:10 +0700

    Refactor Input component className handling

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -1,5 +1,5 @@
+// components/ui/Input.tsx
 import React, { forwardRef } from 'react';
-import { cn } from '@/lib/utils/helpers';
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label?: string;
@@ -19,13 +19,11 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
         )}
         <input
           ref={ref}
-          className={cn(
-            'w-full px-3 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white',
+          className={`w-full px-3 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white ${
             error
               ? 'border-red-500 focus:ring-red-500 focus:border-red-500'
-              : 'border-gray-300 dark:border-gray-600',
-            className
-          )}
+              : 'border-gray-300 dark:border-gray-600'
+          } ${className}`}
           {...props}
         />
         {error && (
